1efaa373a4b3f629dfec4a27ea217c14becb1f04
Jaime Tisnado <jaime.tisnado@gmail.com>
2025-05-22 13:34:55 -0600

    Primer commit

---
+++ mod.includes/footerBar.php
@@ -0,0 +1,12 @@
+<div class="footerBar footerBar_fixed">
+	<div class="container">
+    	<div class="row">
+            <div class="column50">
+                &copy;<?php echo date("Y")?> <a href="javascript:;"><?php echo _metaAutor; ?></a>. Todos los Derechos Reservados
+            </div>
+            <div class="column50 tright">
+                <span class="cBlanco"><?php echo _nomApp; ?> v1.4</span>
+            </div>
+        </div>
+    </div><!-- container -->
+</div><!-- footerBar -->
